3ae3bc1ccbe470d57b902657ae24da21fa5ba4f5
Bersanone <andrea.bersani@itsrizzoli.it>
2025-11-06 11:54:07 +0100

    R

--- prova.py
+++ prova.py
@@ -14,3 +14,62 @@ print('Creiamo delle modifche')
 print('Desso creiam oaltro')
 
 a='qiehfeifhiefiefjeijfioewifjewiojfoiw'
+
+
+
+
+
+#ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddddd
+# ddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# 
+# 
+# 
+# dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+# dddddddddddddddddddddddddddddddddddddddddddd
+# c
+# 
+# d
+# dd
+# d
+# 
+# d
+# d
+# d
+# d
+# dddddddddddddfdsfa
+# gag
+# a
+# 
+# rea
+# 
+# g
+# w
+# trewq
+# t
+# rwe
+# t
+# w
+# t
+# et
+# wq
+# tq
+# wt
+# w 
+# t
+# wqt
+# w
+# t
+# tewt
+# w
+# tew
+# tewd
