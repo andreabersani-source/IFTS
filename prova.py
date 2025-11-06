4ab7681103a9c05f45d11a154d3e925a68d0dcc9
Bersanone <andrea.bersani@itsrizzoli.it>
2025-11-06 11:36:55 +0100

    seconda versione

--- prova.py
+++ prova.py
@@ -3,3 +3,14 @@ for i in range(10):
 
 print('ciao')
 print('mondo')
+
+print('Modifica da registrare su git')
+
+print('Nuovo file python')
+print('Creiamo delle modifche')
+
+
+
+print('Desso creiam oaltro')
+
+a='qiehfeifhiefiefjeijfioewifjewiojfoiw'
